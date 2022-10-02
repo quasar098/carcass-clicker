74da34359427ea3d41241372a1644f4c74a4e249
quasar098 <patellmlg@gmail.com>
2022-10-02 15:23:17 -0400

    daw

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+require("dotenv").config()
 const port = 8080
 
 app.use(express.static('public'))
 
 app.use('*', express.static('not-found'));
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+    console.log(`listening`)
 })
